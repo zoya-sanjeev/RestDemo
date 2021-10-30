cd3fec1204e88cbc33fdb5df50ea622af70dc665
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-30 20:34:07 +0530

    ADDED: initialized project struture

---
+++ src/main/java/com/bridgelabz/myfirstapp/MyFIrstAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.myfirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFIrstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFIrstAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/myfirstapp/MyFIrstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.myfirstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFIrstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
