1e33bc3de2c2ba2c48dec610c351c89a7a4c59eb
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-30 20:57:00 +0530

    ADDED: get request method to pass name as quer parameter created

--- src/main/java/com/bridgelabz/myfirstapp/controller/HelloWorldController.java
+++ src/main/java/com/bridgelabz/myfirstapp/controller/HelloWorldController.java
@@ -1,14 +1,21 @@
 package com.bridgelabz.myfirstapp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hellocontroller")
+@RequestMapping("/hello")
 public class HelloWorldController {
 	@RequestMapping(value = {"", "/", "/home"})
 	public String sayHello() {
 		return "Hello World!";
+	}
+	
+	@RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+	public String sayHello(@RequestParam(value= "name") String name) {
+		return "Hello " + name + "!";
 	}
 
 }
