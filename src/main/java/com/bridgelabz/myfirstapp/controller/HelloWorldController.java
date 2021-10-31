6ab9a484e87b35e92f4ccf1f222b943f5ceaf725
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-31 15:07:37 +0530

    ADDED: put request method to pass first naem as path varibale and last name as query parameter

--- src/main/java/com/bridgelabz/myfirstapp/controller/HelloWorldController.java
+++ src/main/java/com/bridgelabz/myfirstapp/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.bridgelabz.myfirstapp.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -32,6 +33,11 @@ public class HelloWorldController {
 	@PostMapping("/post")
 	public String sayHello(@RequestBody User user) {
 		return "Hello " + user.getFirstName() + " " + user.getLastName() + "!"; 
+	}
+	
+	@PutMapping("put/{firstName}")
+	public String sayHello(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName) {
+		return "Hello " + firstName + " " + lastName + "!";
 	}
 
 }
