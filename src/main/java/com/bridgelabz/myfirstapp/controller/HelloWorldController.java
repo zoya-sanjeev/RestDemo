5386a4696eb210f380d5d3db9752b58e99fd9706
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-31 14:07:40 +0530

    ADDED: get request method to pass name as path variable

--- src/main/java/com/bridgelabz/myfirstapp/controller/HelloWorldController.java
+++ src/main/java/com/bridgelabz/myfirstapp/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.myfirstapp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +17,11 @@ public class HelloWorldController {
 	
 	@RequestMapping(value = {"/query"}, method = RequestMethod.GET)
 	public String sayHello(@RequestParam(value= "name") String name) {
+		return "Hello " + name + "!";
+	}
+	
+	@GetMapping("/param/{name}")
+	public String sayHelloParam(@PathVariable String name) {
 		return "Hello " + name + "!";
 	}
 
